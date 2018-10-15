df6cf0d45259cbd66d6cbcdf3c38a73e54ea105c
Hironobu Komiya <komiya@kailash-tech.jp>
2018-10-16 00:39:07 +0900

    setup.sh 更新

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ for file in .bash_profile .vimrc .gitconfig .screenrc .tmux.conf; do
   curl -s -o "${HOME}/${file}" https://raw.githubusercontent.com/hironobuk/dotfiles/master/${file}
 done
 
+if [[ -n "$(which npm)" ]]; then
+  npm completion > "${HOME}/.npm-completion.bash"
+fi
+
 # --list-cmdsオプションが存在しないと怒られるのでしばらくはリビジョンを固定
 curl -s -o "${HOME}/.git-completion.bash" https://raw.githubusercontent.com/git/git/8b4c2e0b1c4ff47c07b2c1bbc937bb46e889755e/contrib/completion/git-completion.bash
 curl -s -o "${HOME}/.git-prompt.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
