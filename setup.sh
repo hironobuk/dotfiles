33a9d04149124c18d7e3ff1d18a442d53ec1e98e
Hironobu Komiya <komiya@kailash-tech.jp>
2020-03-26 00:15:32 +0900

    ng-template.vim を .vim/ に移動

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ if [[ -n "$(which npm)" ]]; then
 fi
 
 mkdir -p "${HOME}/.vim/after/syntax"
-curl -s -o "${HOME}/.vim/after/syntax/html.vim" https://raw.githubusercontent.com/hironobuk/dotfiles/master/ng-template.vim
+curl -s -o "${HOME}/.vim/after/syntax/html.vim" https://raw.githubusercontent.com/hironobuk/dotfiles/master/.vim/ng-template.vim
 
 curl -s -o "${HOME}/.git-completion.bash" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -s -o "${HOME}/.git-prompt.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
